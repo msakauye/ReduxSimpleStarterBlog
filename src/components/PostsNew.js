066a162b206c931a6f8e506c620be29263a9e1e3
Mark Sakauye <msakauye@uthsc.edu>
2018-02-16 21:50:55 -0500

    new post form

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label htmlFor={field.input.name}>{field.label}</label>
+                <input 
+                    type="text"
+                    className="form-control" 
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
-                <Link to="/">&lt; Go back to posts</Link>
+                <div>
+                    <Link to="/">&lt; Go back to posts</Link>
+                </div>
+                <form>
+                    <Field
+                        name="title"
+                        label="Title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        name="categories"
+                        label="Categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        name="content"
+                        label="Content"
+                        component={this.renderField}
+                    />
+                </form>
             </div>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
